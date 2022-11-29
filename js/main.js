b90f33218dbef8887650cbbaa819a86db97100f9
Matthew Powell <matt.powell2784@gmail.com>
2022-11-29 20:01:14 +0000

    movie nav added using dom manipulation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
 'use strict'
+import { cleanMovieData } from './movieData.js'
+import { renderMovieNav } from './renderMovieNav.js'
+
+renderMovieNav(cleanMovieData)
--- js/movieData.js
+++ js/movieData.js
@@ -1,10 +1,11 @@
-export const movieData = {
+const movieData = {
   'The Darjeeling Limited': {
     plot: "A year after their father's funeral, three brothers travel across India by train in an attempt to bond with each other.",
     cast: ['Jason Schwartzman', 'Owen Wilson', 'Adrien Brody'],
     runtime: 151,
     rating: 7.2,
     year: 2007,
+    image: './img/01_the_darjeeling.jpg',
   },
   'The Royal Tenenbaums': {
     plot: 'The eccentric members of a dysfunctional family reluctantly gather under the same roof for various reasons',
@@ -12,6 +13,7 @@ export const movieData = {
     year: 2001,
     cast: ['Gene Hackman', 'Gwnyeth Paltrow', 'Anjelica Huston'],
     runtime: 170,
+    image: './img/02_the_royal_tenenbaums.jpg',
   },
   'Fantastic Mr. Fox': {
     year: 2009,
@@ -24,6 +26,7 @@ export const movieData = {
     ],
     runtime: 147,
     rating: 7.9,
+    image: './img/03_fantastic_mr_fox.jpg',
   },
   'The Grand Budapest Hotel': {
     rating: 8.1,
@@ -31,5 +34,23 @@ export const movieData = {
     year: 2014,
     plot: "A writer encounters the owner of an aging high-class hotel, who tells him of his early years serving as a lobby boy in the hotel's glorious years under an exceptional concierge.",
     cast: ['Ralph Fiennes', 'F. Murray Abraham', 'Mathieu Amalric'],
+    image: './img/04_budapest_hotel.jpg',
   },
 }
+
+const movieDataArray = Object.entries(movieData)
+export const cleanMovieData = []
+
+movieDataArray.forEach((movie, key) => {
+  const cleanMovie = {
+    key: key,
+    title: movie[0],
+    plot: movie[1].plot,
+    cast: movie[1].cast,
+    runtime: movie[1].runtime,
+    rating: movie[1].rating,
+    year: movie[1].year,
+    image: movie[1].image,
+  }
+  cleanMovieData.push(cleanMovie)
+})
