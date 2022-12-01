1227376a45f012d212bbfce6c18a9fe4f9e659a2
Matthew Powell <matt.powell2784@gmail.com>
2022-12-01 19:12:12 +0000

    toggle nodes code refactor

--- js/eventListeners.js
+++ js/eventListeners.js
@@ -4,10 +4,15 @@ import { formNode } from './toogleNodes/nodeProps.js'
 import { onFormSubmit } from './helpers/onFormSubmit.js'
 
 export const addEventListeners = () => {
-  const addMoiveNav = document.getElementById('add_moive')
-  addMoiveNav.addEventListener('click', () => {
+  const addMovie = document.getElementById('add_moive')
+  addMovie.addEventListener('click', () => {
     toggleNode(movieNavNode, false)
     toggleNode(formNode, true)
+  })
+
+  const hideMoiveBrowser = document.getElementById('hide_moive_browser')
+  hideMoiveBrowser.addEventListener('click', () => {
+    toggleNode(movieNavNode, true)
   })
 
   const form = document.querySelector('form')
