211735cdbcc846cf0ffcea253da972949ea4c123
Matthew Powell <matt.powell2784@gmail.com>
2022-12-08 19:28:59 +0000

    insert html testing

--- js/domManipulation/insertHtmlFile/insertInitialHtmlFiles.js
+++ js/domManipulation/insertHtmlFile/insertInitialHtmlFiles.js
@@ -2,8 +2,8 @@ import { insertHtmlFile } from './insertHtmlFile.js'
 import { header, homePage, homePageBgImage, moiveNav } from './htmlFileProps.js'
 
 export const insertInitialHtmlFiles = async () => {
+  await insertHtmlFile(moiveNav)
   await insertHtmlFile(header)
   await insertHtmlFile(homePage)
   await insertHtmlFile(homePageBgImage)
-  await insertHtmlFile(moiveNav)
 }
