a9ca025712a4471694d97bb5ddf9044de5e5f207
Matthew Powell <matt.powell2784@gmail.com>
2022-12-08 19:17:26 +0000

    insert html testing

--- js/domManipulation/insertHtmlFile/insertHtmlFile.js
+++ js/domManipulation/insertHtmlFile/insertHtmlFile.js
@@ -1,11 +1,11 @@
-export const insertHtmlFile = async ({
-  htmlFile,
-  containerElementId,
-  htmlInsertPosition,
-}) => {
-  const getHtml = await fetch(htmlFile)
-  const htmlToBeInserted = await getHtml.text()
+// export const insertHtmlFile = async ({
+//   htmlFile,
+//   containerElementId,
+//   htmlInsertPosition,
+// }) => {
+//   const getHtml = await fetch(htmlFile)
+//   const htmlToBeInserted = await getHtml.text()
 
-  const htmlInsertElement = document.getElementById(containerElementId)
-  htmlInsertElement.insertAdjacentHTML(htmlInsertPosition, htmlToBeInserted)
-}
+//   const htmlInsertElement = document.getElementById(containerElementId)
+//   htmlInsertElement.insertAdjacentHTML(htmlInsertPosition, htmlToBeInserted)
+// }
