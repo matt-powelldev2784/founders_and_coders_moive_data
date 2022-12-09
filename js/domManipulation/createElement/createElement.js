2a2115131dc366df9eba32c988bc27edcc4970df
Matthew Powell <matt.powell2784@gmail.com>
2022-12-09 17:05:07 +0000

    create element function added

---
+++ js/domManipulation/createElement/createElement.js
@@ -0,0 +1,12 @@
+const createRichElement = (tagName, attributes, ...content) {
+  let element = document.createElement(tagName)
+  if (attributes) {
+    for (const [attr, value] of Object.entries(attributes)) {
+      element.setAttribute(attr, value)
+    }
+  }
+  if (content && content.length) {
+    element.append(...content)
+  }
+  return element
+}
