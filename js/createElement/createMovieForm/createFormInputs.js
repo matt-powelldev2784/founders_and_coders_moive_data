d0af41183a8bd7b86066631b4830a52c82bf331c
Matthew Powell <matt.powell2784@gmail.com>
2022-12-14 21:46:33 +0000

    add movie form update

--- js/createElement/createMovieForm/createFormInputs.js
+++ js/createElement/createMovieForm/createFormInputs.js
@@ -1,6 +1,6 @@
-import { createFormInput } from './createFormInput.js'
-import { createElement } from '../createElement.js'
-import { createSmallInputs } from './createSmallInputs.js'
+import { createFormInput } from './createFormInput.js';
+import { createElement } from '../createElement.js';
+import { createSmallInputs } from './createSmallInputs.js';
 
 export const createFormInputs = () => {
   const title = createFormInput('input', {
@@ -9,9 +9,9 @@ export const createFormInputs = () => {
     name: 'title',
     placeholder: 'Title',
     required: 'required',
-  })
+  });
 
-  const smallInputs = createSmallInputs()
+  const smallInputs = createSmallInputs();
 
   const plot = createFormInput('textarea', {
     type: 'text',
@@ -19,31 +19,46 @@ export const createFormInputs = () => {
     name: 'plot',
     placeholder: 'Plot',
     required: 'required',
-  })
+  });
 
   const cast = createFormInput('input', {
     type: 'text',
     class: 'form_input__text',
     name: 'cast',
-    placeholder: 'Cast ',
+    placeholder: 'Cast (comma separated)',
     required: 'required',
-  })
+  });
+
+  const fileLabelImg = createElement('img', {
+    class: 'form__input_file_img',
+    src: './img/upload_image.svg',
+    alt: 'Upload image',
+  });
+
+  const fileLabelText = createElement('p', { class: 'form__input_file_text' }, 'Add image...');
 
   const file = createFormInput('input', {
     type: 'file',
     id: 'form__image_upload',
     accept: 'image/jpeg, image/png, image/jpg',
-    class: 'form_input__text',
+    class: 'form_input__file',
     name: 'image',
     placeholder: 'Upload Image',
     required: 'required',
-  })
+  });
+
+  const fileLabel = createElement(
+    'label',
+    { class: 'form__input_file_label', for: 'form__image_upload' },
+    fileLabelImg,
+    fileLabelText
+  );
 
   const submitBtn = createElement('input', {
     class: 'form__submit_button',
     type: 'submit',
     value: 'Submit',
-  })
+  });
 
   const formInputs = createElement(
     'div',
@@ -52,9 +67,11 @@ export const createFormInputs = () => {
     smallInputs,
     plot,
     cast,
+    fileLabel,
     file,
-    submitBtn
-  )
 
-  return formInputs
-}
+    submitBtn
+  );
+
+  return formInputs;
+};
--- js/createElement/createMovieForm/createSmallInputs.js
+++ js/createElement/createMovieForm/createSmallInputs.js
@@ -1,36 +1,36 @@
-import { createFormInput } from './createFormInput.js'
-import { createElement } from '../createElement.js'
+import { createFormInput } from './createFormInput.js';
+import { createElement } from '../createElement.js';
 
 export const createSmallInputs = () => {
   const year = createFormInput('input', {
     type: 'number',
     class: 'form_input__number',
     name: 'year',
-    placeholder: 'Year',
+    placeholder: 'Year YYYY',
     required: 'required',
     min: '1801',
     max: '3000',
-  })
+  });
 
   const rating = createFormInput('input', {
     type: 'number',
     class: 'form_input__number',
     name: 'rating',
-    placeholder: 'Rating',
+    placeholder: 'Rating (out of 10)',
     required: 'required',
     min: '1',
     max: '10',
-  })
+  });
 
   const runTime = createFormInput('input', {
     type: 'number',
     class: 'form_input__number',
     name: 'runtime',
-    placeholder: 'Run Time',
+    placeholder: 'Run Time (mins)',
     required: 'required',
     min: '1',
     max: '999',
-  })
+  });
 
   const smallInputsWrapper = createElement(
     'div',
@@ -40,7 +40,7 @@ export const createSmallInputs = () => {
     year,
     rating,
     runTime
-  )
+  );
 
-  return smallInputsWrapper
-}
+  return smallInputsWrapper;
+};
