027d2bad0392a8ee031af1a8ee704fd0c7c8acfc
Matthew Powell <matt.powell2784@gmail.com>
2022-12-16 20:41:32 +0000

    homepage text overflow fix

--- js/createElement/createHomePage/createHomeText.js
+++ js/createElement/createHomePage/createHomeText.js
@@ -1,26 +1,12 @@
-import { createElement } from '../createElement.js'
+import { createElement } from '../createElement.js';
 
 export const createHomeText = () => {
-  const h2 = createElement(
-    'h2',
-    { class: 'homepage_text__h2' },
-    'Use the menu to browse the latest movies...'
-  )
+  const h2 = createElement('h2', { class: 'homepage_text__h2' }, 'Use the menu to browse the latest movies...');
 
-  const span = createElement('span', { class: 'homepage_text__h1_span' }, 'YOU')
-  const h1 = createElement(
-    'h1',
-    { class: 'homepage_text__h1' },
-    'MOVIE PREVIEWS FOR ',
-    span
-  )
+  const span = createElement('span', { class: 'homepage_text__h1_span' }, 'YOU');
+  const h1 = createElement('h1', { class: 'homepage_text__h1' }, 'MOVIE PREVIEWS FOR ', span);
 
-  const textHeaderDiv = createElement(
-    'div',
-    { class: 'homepage_text__header_div' },
-    h1,
-    h2
-  )
+  const textHeaderDiv = createElement('div', { class: 'homepage_text__header_div' }, h1, h2);
 
   const article = createElement(
     'article',
@@ -29,7 +15,7 @@ export const createHomeText = () => {
       id: 'homepage_text__flexbox',
     },
     textHeaderDiv
-  )
+  );
 
   const section = createElement(
     'section',
@@ -38,7 +24,7 @@ export const createHomeText = () => {
       id: 'homepage_text',
     },
     article
-  )
+  );
 
-  return section
-}
+  return section;
+};
